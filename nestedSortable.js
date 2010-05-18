abf6a81d665fc89e9cc91fdd9294b284d9e6f816
Simon McManus <mcmanus.simon@gmail.com>
2010-05-18 18:02:43 +0100

    updates to nestedSrtable

--- nestedSortable.js
+++ nestedSortable.js
@@ -1,46 +1,43 @@
-
-(function($){  
-	$.fn.smmNestedSortable = function() {  
-		var defaults = {  
-			placeholderClass: "placeholder" 
-		};
-		var options = $.extend(defaults, options);  
-		$(this).sortable({
-			items: "li",
-			helper: "helper",
-			placeholder:'placeholder',
-			revert: true,
-			forceHelperSize: true,
-			sort: function(event, ui){
-				// ui.helper - item being dragged
- 				if($(ui.placeholder).children().length <= 0){
-			//				console.log("ui", ui, event, this);
-						//	var frag  = document.createDocumentFragment();
-						//	$(frag).append(ui.helper);
-					
-					$(ui.placeholder).append("<div class='"+defaults.placeholderClass+"'>"+$(ui.helper).html()+"</div>");
-//					$(ui.placeholder).append($(ui.item));
-//					console.log("ui", ui.helper);
-//					console.log(frag);
-				}
-			},
-		   stop: function(event, ui) {
-				$.fn.smmNestedSortable.change(event, ui);
-			}
-	});
-	$.fn.smmNestedSortable.change= function(event, ui) {
-				if(ui.position.left-19  >  ui.originalPosition.left){
-					
-					console.log($(ui.placeholder).parent());
-//					if(!$(ui.item).is("ul.sortable")){
-						$(ui.item).wrap("<li><ul class='droppable'/></li>");
-//					}
-				}else if(ui.position.left-19  <  ui.originalPosition.left){
-					// kill child
-					if($(ui.item).parent().parent().is("li")){
-						$(ui.item).parent().parent().replaceWith($(ui.item));
-					}
-				}
-	}
+(function($) {
+    $.fn.smmNestedSortable = function() {
+        var defaults = {
+            placeholderClass: "placeholder"
+        };
+        var options = $.extend(defaults, options);
+        $.fn.smmNestedSortable.apply(this);
+    };
+    $.fn.smmNestedSortable.change = function(event, ui) {
+        if (ui.position.left - 19 > ui.originalPosition.left) {
+            console.log('cl', $(ui.item));
+            //					$(ui.item).wrap("<li><ul class='droppable'/></li>");
+            $(ui.item).wrapAll("<li><ul class='sortable'></ul></li>");
+            //					console.log($(ui.item).wrapInner("<li></li>"));
+            //					if(!$(ui.item).is("ul.sortable")){
+            //						$(ui.item).wrap("<li><ul class='droppable'/></li>");
+            //					}
+        } else if (ui.position.left - 19 < ui.originalPosition.left) {
+            // kill child
+            if ($(ui.item).parent().parent().is("li")) {
+                $(ui.item).parent().parent().replaceWith($(ui.item));
+            }
+        }
+   }
+ 	$.fn.smmNestedSortable.apply = function(sortable) {
+    $(sortable).sortable({
+        items: "li",
+        helper: "helper",
+        placeholder: 'placeholder',
+        revert: true,
+        forceHelperSize: true,
+        sort: function(event, ui) {
+            if ($(ui.placeholder).children().length <= 0) {
+                $(ui.placeholder).append("<div class='sortable'>" + $(ui.helper).html() + "</div>");
+                //					$(ui.placeholder).append($(ui.item)); // should work but does not - probably becuse the ui.item is being dragged?!?
+            }
+        },
+        stop: function(event, ui) {
+            $.fn.smmNestedSortable.change(event, ui);
+        }
+    });
 };
-})(jQuery);  
+})(jQuery);
