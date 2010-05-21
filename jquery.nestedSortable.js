a2224bb2e73359ccacc5175ccd45120b3148221a
Simon McManus <mcmanus.simon@gmail.com>
2010-05-21 10:10:03 +0100

    adding connectWith param to sortables allows dragging both ways between lists

--- jquery.nestedSortable.js
+++ jquery.nestedSortable.js
@@ -5,18 +5,18 @@
  		$(this).sortable({
 			items: "li",
 			helper: "helper",
+			connectWith: '.sortable',
+			
 			placeholder: 'placeholder',
-			revert: true,
-			forceHelperSize: true,
 			sort: function(event, ui) {
-		//		if ($(ui.placeholder).children().length <= 0) {
-		//			$(ui.placeholder).append("<div class='sortable'>" + $(ui.helper).html() + "</div>");
-		//			// $(ui.placeholder).append($(ui.item)); // should work but does not - probably becuse the ui.item is busy being dragged?!?
-		//		}
+			///	if ($(ui.placeholder).children().length <= 0) {
+			//		$(ui.placeholder).append("<div class='sortable'>" + $(ui.helper).html() + "</div>");
+			//		// $(ui.placeholder).append($(ui.item)); // should work but does not - probably becuse the ui.item is busy being dragged?!?
+			//	}
 			},
 			stop: function(event, ui) {
-				$.fn.smmNestedSortable.change(event, ui);
-				$('.sortable').sortable( "refresh" );
+			//	$.fn.smmNestedSortable.change(event, ui);
+			//	$('.sortable').sortable( "refresh" );
 			}
 		});
     });
@@ -33,13 +33,10 @@
            // kill child
 		// confirm its not the base level element	
 			
-           if ($(ui.item).parent().is("ul")) {
+           if ($(ui.item).parent().parent().is("li")) {
 			// if only item in the list
-			if($(ui.item).parent().children().length > 1){ // more than one item in the list
-				console.log('here');
-				 $(ui.item).insertBefore("</ul>");
-			}else{
-				$(ui.item).parent().replaceWith($(ui.item));
+			if($(ui.item).parent().parent().children().length < 1){ // more than one item in the list
+				$(ui.item).parent().parent().replaceWith($(ui.item));
 			}
            }
        }
