505138f35909b669f736dc97ed951967b7069a5d
Simon McManus <mcmanus.simon@gmail.com>
2010-05-21 16:22:39 +0100

    adding css goodness

--- jquery.nestedSortable.js
+++ jquery.nestedSortable.js
@@ -7,21 +7,22 @@
 			connectWith: '.sortable',
 			placeholder: 'placeholder',
 			sort: function(event, ui) {
-				ui.item = ui.placeholder;
-				$.fn.smmNestedSortable.change(event, ui);
+				$(ui.placeholder).empty();
+				if (ui.position.left - 10 > ui.originalPosition.left) 	// make child 
+					$(ui.placeholder).append("<ul class='sortable'>"+$(ui.item).html()+"</ul>");
 			},
 			stop: function(event, ui) {
 				$.fn.smmNestedSortable.change(event, ui);
-			}
+			}, 
 		});
     });
     return this;
   };
   	$.fn.smmNestedSortable.change = function(event, ui) {
-		if (ui.position.left - 20 > ui.originalPosition.left) { 	// make child - could/should add some more check in here. 
+		if (ui.position.left - 10 > ui.originalPosition.left) { 	// make child - could/should add some more check in here. 
 			if($(ui.placeholder).parent().children().length <= 1)
 				$(ui.item).wrapAll("<li><ul class='sortable'></ul></li>");
-       } else if (ui.position.left - 20 < ui.originalPosition.left) {  // kill child
+       } else if (ui.position.left - 10 < ui.originalPosition.left) {  // kill child
           if ($(ui.item).parent().parent().is("ul")) {		// confirm its not the base level element	
 				if($(ui.item).parent().parent().children().length <= 1){ // if only item in the list
 					$(ui.item).parent().parent().replaceWith($(ui.item));
